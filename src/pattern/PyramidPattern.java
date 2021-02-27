a567f477543120adcf3b763f30b7772065e657d6
Shreyansh Jain <shreyansh791@yahoo.in>
2021-02-27 09:18:54 +0530

    pyramid pattern program added

---
+++ src/pattern/PyramidPattern.java
@@ -0,0 +1,21 @@
+package pattern;
+
+public class PyramidPattern {
+public static void main(String[] args) {
+	for(int i=5;i>=1;i--)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			if(i>j)
+			{
+				System.out.print(" ");
+			}
+			else
+			{
+				System.out.print("* ");
+			}
+		}	
+		System.out.println();
+	}
+}
+}
